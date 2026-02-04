64b1fdc1bb8f71f959bac4027f107e11282c3e5c
ZNAP <258827052+znap-dev@users.noreply.github.com>
2026-02-04 06:57:09 +0300

    Update api.js
    update rate limit

--- api.js
+++ api.js
@@ -184,7 +184,7 @@ async function rateLimiter(req, res, next) {
   next();
 }
 
-app.use(rateLimiter);
+// app.use(rateLimiter); // Rate limit devre dışı
 
 // ============================================
 // DATABASE
